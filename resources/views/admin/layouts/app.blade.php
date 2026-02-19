41b87f5b435396787a6b5ab912d3ef7319562feb
Guilhermo <guilherme.fernandes@smtzeventos.com.br>
2026-02-19 14:16:46 -0300

    Update app.blade.php
    Correção da ordem do JS

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -10,8 +10,8 @@
     <link rel="stylesheet" href="{{ asset('assets/theme.css') }}">
 
     {{-- Alpine (accordion) --}}
-    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script defer src="https://unpkg.com/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     <style>[x-cloak]{display:none!important}</style>
 </head>
